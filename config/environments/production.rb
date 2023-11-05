834a4b59cfb591e9c844f97558d4fa738a6dda1e
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2023-11-05 12:16:31 +0900

    `bin/rails generate devise:install`を実行してセットアップ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Set Url Options
+  # TODO: Set your host name
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
