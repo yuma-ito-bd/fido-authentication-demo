da7dc2e22b7afc69e783b314cb10e0c711274ff5
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2023-11-05 23:18:56 +0900

    `@simplewebauthn/browser`npmパッケージを削除
    `startRegistration`でサーバーから受け取ったパラメータを渡すと、`User ID was not between 1 and 64 characters`というエラーが発生してしまった。

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@simplewebauthn/browser", to: "https://ga.jspm.io/npm:@simplewebauthn/browser@8.3.4/dist/bundle/index.js"
