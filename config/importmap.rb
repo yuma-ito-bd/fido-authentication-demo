ed24f787c91f0fedb0e49e1a52b9a4d96a3721fd
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2023-11-05 23:20:17 +0900

    `@github/webauthn-json`に変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "@github/webauthn-json", to: "https://ga.jspm.io/npm:@github/webauthn-json@2.1.1/dist/esm/webauthn-json.js"
