53855c2d2c813f8ebe9370c77b60ccc58c554032
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2023-11-05 16:24:27 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_for :users
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root "tops#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    post 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
+    post 'sign_in/new_challenge', to: 'users/sessions#new_challenge', as: :new_user_session_challenge
+  end
 end
